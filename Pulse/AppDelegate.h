dfdcf04a01febfbc49fd2332106c10587eb87cf1
Sainath <alekyareddy@Sainaths-Mac.local>
2017-10-23 10:16:16 -0700

    Initial Commit

---
+++ Pulse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pulse
+//
+//  Created by Sainath on 23/10/17.
+//  Copyright © 2017 Sainath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pulse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pulse
+//
+//  Created by Sainath on 23/10/17.
+//  Copyright © 2017 Sainath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
